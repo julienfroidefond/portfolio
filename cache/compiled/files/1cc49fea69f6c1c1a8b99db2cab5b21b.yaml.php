4772921c1f01bdbf1b86fcc4d76350ea8a089e14
Administrator <julienfroidefond@gmail.com>
2017-07-28 11:11:54 +0200

    port 587 mailgun

--- cache/compiled/files/1cc49fea69f6c1c1a8b99db2cab5b21b.yaml.php
+++ cache/compiled/files/1cc49fea69f6c1c1a8b99db2cab5b21b.yaml.php
@@ -2,22 +2,18 @@
 return [
     '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
     'filename' => '/Users/julienfroidefond/Sites/portfolio/user/config/plugins/email.yaml',
-    'modified' => 1501231202,
+    'modified' => 1501232962,
     'data' => [
         'enabled' => true,
         'from' => 'julienfroidefond@gmail.com',
-        'to' => 'julienfroidefond@gmail.com',
         'mailer' => [
             'engine' => 'mail',
             'smtp' => [
                 'server' => 'smtp.mailgun.org',
-                'port' => 25,
+                'port' => 587,
                 'encryption' => 'none',
-                'user' => 'postmaster@sandbox4071c121181a409b988e28c1c165a398.mailgun.org',
-                'password' => '1435a671e587c7a8f7cdb21edd6b2405'
-            ],
-            'sendmail' => [
-                'bin' => '/usr/sbin/sendmail'
+                'user' => 'postmaster@sandbox604a727076ff46a9bf2484fbaf987918.mailgun.org',
+                'password' => '98da47b81f2197048bfe03bb89d4a469'
             ]
         ]
     ]
