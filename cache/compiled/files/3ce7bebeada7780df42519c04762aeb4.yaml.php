6d1341a0e00d0bbb7d5b66d3441361431a260437
Administrator <julienfroidefond@gmail.com>
2017-07-28 10:51:20 +0200

    cleaning ignore

--- cache/compiled/files/3ce7bebeada7780df42519c04762aeb4.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/julienfroidefond/Sites/landio/user/config/streams.yaml',
-    'modified' => 1500724062,
-    'data' => [
-        
-    ]
-];
--- cache/compiled/files/88a6b3fb92dba7a77ccb30ac86ee81d1.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'themes://landio/landio.yaml',
-    'modified' => 1500445200,
-    'data' => [
-        'enabled' => true
-    ]
-];
